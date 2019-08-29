33617c94f151cf7df037d2d27ba252722e39e2e5
Sumeet <sumeet@friday>
2019-08-29 13:13:17 +0530

    fixed color

--- lib/utils/widgets.dart
+++ lib/utils/widgets.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 
 List<Color> colors = [
-//  Color(0xffb0bec5),
   Color(0xffecf0f1),
   Color(0xfffff3e0),
   Color(0xffdcedc8),
   Color(0xffb2ebf2),
   Color(0xffb3e5fc),
   Color(0xfffce4ec),
-  Color(0xffdcedc8),
+  Color(0xffb0bec5),
   Color(0xfffffde7),
   Color(0xffefebe9),
 ];
